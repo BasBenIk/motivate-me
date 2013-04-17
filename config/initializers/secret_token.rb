8c184abb98e22ceb320e2216e164a72e604e53af
Bas van Baalen <bas@brightin.nl>
2013-04-17 13:25:40 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MotivateMe::Application.config.secret_token = 'a6b5b7d19e51a75c0761b9c3e09ff21bb2e8f2bad68ce2648d1b702abc43e708dc0f0bcf6901e2a44bddf5b4d626666db0f3bb44c072857a949f0977d79f555d'
