f34ee96b00ac085ea264a12ebddf45614fae5391
Sebastiaan Scheers <zwooosh@gmail.com>
2013-04-22 10:57:34 +0200

    add nifty_generators, add friends

--- Gemfile
+++ Gemfile
@@ -23,8 +23,11 @@ group :development do
   gem "better_errors", ">= 0.7.2"
   gem "binding_of_caller", ">= 0.7.1", :platforms => [:mri_19, :rbx]
   gem 'meta_request'
+  gem "nifty-generators"
 end
 
 group :production do
   gem 'pg'
 end
+
+gem "mocha", :group => :test
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :username, :date_of_birth, :gender
   has_and_belongs_to_many :groups
   has_and_belongs_to_many :goals
+  has_many :friendships
+  has_many :friends , :through => :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
 end
